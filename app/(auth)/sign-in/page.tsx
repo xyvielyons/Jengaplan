01167dadfad2003745da1b151926b5574008d8cc
xyvie lyons <xyvielyons@gmail.com>
2025-02-02 03:49:02 +0300

    login functionality

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import Image from 'next/image'
-import SignUpForm from '@/components/forms/SignUpForm'
 import Link from 'next/link'
+import SignInForm from '@/components/forms/SignInForm'
 import Socials from '@/components/global/Socials'
 // import { assets } from '@/public/assets'
 const page = () => {
   return (
-    <div className="flex flex-col md:flex-row mt-[32px] p-4 relative">
-      <div className="bg-lightradial dark:bg-darkradial w-[1000px] h-[1000px] absolute right-0 transform translate-x-[55%] top-[-200px] hidden md:block"></div>
+    <div className="flex flex-col md:flex-row mt-[32px] p-4 relative h-screen">
+            <div className="bg-lightradial dark:bg-darkradial w-[1000px] h-[1000px] absolute right-0 transform translate-x-[55%] top-[-200px] hidden md:block"></div>
 
       {/* Left Section */}
       <div className="pt-6 pb-2 px-4 space-y-[28px] md:w-1/2 md:p-8 lg:p-16">
         <div>
-          <h1 className="text-4xl font-bold text-gray-800 dark:text-slate-100">Create account</h1>
+          <h1 className="text-4xl font-bold text-gray-800 dark:text-slate-100">Welcome Back!</h1>
           <p className="text-gray-600 dark:text-slate-300">
-            Effortless Member Management Starts Here – Create Your Account Today!
+          Manage Your Memberships with Ease.
           </p>
-          <SignUpForm />
+          <SignInForm />
         </div>
         <div className="">
           <Socials></Socials>
         </div>
         <div className="">
-          <span className='text-sm text-gray-600 dark:text-slate-300'>Have an account? <Link href="/sign-in" className='text-gray-800 dark:text-slate-50 font-semibold hover:text-gray-500 active:text-gray-500'>Login</Link></span>
+          <span className='text-sm text-gray-600 dark:text-slate-300'>Dont have an account? <Link href="/sign-up" className='text-gray-800 dark:text-slate-50 font-semibold hover:text-gray-500 active:text-gray-500'>Register</Link></span>
         </div>
       </div>
 
@@ -30,7 +30,7 @@ const page = () => {
       <div className="md:w-1/2 w-full flex items-center p-[8px]">
         <div className="w-full max-w-[600px] p-[4px] relative rounded-sm ">
           <Image
-            src="https://drive.google.com/uc?export=view&id=1_GBoP8UB_oRGFT-ZmrfktlUtQ5iH5s2Q"
+            src="https://drive.google.com/uc?export=view&id=136me9g1mcmQa3yoNm9j7-oPiSPIN4A8S"
             alt="sign up image"
             layout="intrinsic"
             width={1800}
@@ -39,7 +39,7 @@ const page = () => {
           />
           <div className="text-white absolute bottom-[50px] p-[20px] bg-black/80">
             <h1 className='text-[20px] font-bold'>JengaScheme</h1>
-            <p className='text-[16px] wrap '>Built to Save Teachers Time</p>
+            <p className='text-[16px] wrap '>Your Partner in Effortless Lesson Planning</p>
           </div>
         </div>
       </div>
---
+++ components/forms/SignInForm.tsx
@@ -0,0 +1,104 @@
+'use client'
+import React,{useState} from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from '@heroui/react'
+import { useRouter } from 'next/navigation'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { signInSchema } from '@/zod/zod'
+import { authClient } from '@/auth-client'
+import { useToast } from "@/hooks/use-toast"
+import Link from 'next/link'
+const SignInForm = () => {
+    const [pending, setPending] = useState(false);
+    const { toast } = useToast()
+    const router = useRouter()
+        // 1. Define your form.
+        const form = useForm<z.infer<typeof signInSchema>>({
+          resolver: zodResolver(signInSchema),
+          defaultValues: {
+            email:"",
+            password:"",
+          },
+        })
+        const {reset} = form;
+       
+        // 2. Define a submit handler.
+        async function onSubmit(values: z.infer<typeof signInSchema>) {
+            await authClient.signIn.email(
+                {
+                    email: values.email,
+                    password: values.password,
+                },
+                {
+                    onRequest:()=>{
+                        setPending(true);
+                    },
+                    onSuccess:()=>{
+                        router.push("/");
+                        router.refresh();
+                    },
+                    onError:(ctx)=>{
+                        console.log("error",ctx);
+                        toast({
+                            title: "Something went wrong",
+                            description: ctx.error.message ?? "Something went wrong.",
+                            variant:"destructive"
+                        });
+                    }
+                }
+            )
+            reset()
+            setPending(false);
+        }
+      
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='font-bold text-[13px] text-slate-600 dark:text-slate-300'>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="johndoe@example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='font-bold text-[13px] text-slate-600 dark:text-slate-300'>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Atleast 8 characters" {...field} type="password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <div className="flex justify-end">
+           <Link href="/forgot-password" className='text-gray-500 font-medium hover:text-gray-700 active:text-gray-700 text-sm dark:text-gray-300 dark:hover:text-gray-50'>Forgot password?</Link>
+        </div>
+        <Button isLoading={pending} className='w-full text-[15px] bg-[#007AFF] font-medium text-white' type="submit" radius='sm'>Sign In</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default SignInForm
