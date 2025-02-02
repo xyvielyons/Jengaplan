afd22745248bdf2973a0f25a130cf761cd34838f
xyvie lyons <xyvielyons@gmail.com>
2025-02-02 06:14:43 +0300

    reset password functionality

---
+++ app/(auth)/reset-password/page.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Image from "next/legacy/image"
+import Link from 'next/link'
+import ResetPassword from '@/components/forms/ResetForm'
+const page = () => {
+  return (
+    <div className="flex flex-col md:flex-row mt-[32px] p-4 relative h-full">
+              <div className="bg-lightradial dark:bg-darkradial w-[1000px] h-[1000px] absolute right-0 transform translate-x-[55%] top-[-200px] hidden md:block"></div>
+
+      {/* Left Section */}
+      <div className="pt-6 pb-2 px-4 space-y-[28px] md:w-1/2 md:p-8 lg:p-16">
+
+        <div>
+          <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-100">Change Password</h1>
+          <p className="text-gray-600 dark:text-gray-200">
+          Please input your new password
+          </p>
+          <ResetPassword />
+        </div>
+        <div className="">
+          <span className='text-sm text-gray-600 dark:text-gray-300'>Dont have an account? <Link href="/sign-up" className='text-gray-800 font-semibold hover:text-gray-500 active:text-gray-500 dark:text-gray-50 dark:hover:text-gray-600'>Register</Link></span>
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <div className="md:w-1/2 w-full flex items-center justify-center p-[16px]">
+        <div className="w-full max-w-[600px] p-8  relative">
+          <Image
+            src="https://drive.google.com/uc?export=view&id=1hJne2MAJIXWeCB_d7z-Xg956B3OpJhAo"
+            alt="reset image"
+            layout="intrinsic"
+            width={1000}
+            height={1000}
+            className="object-cover rounded-lg "
+          />
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ components/forms/ResetForm.tsx
@@ -0,0 +1,137 @@
+'use client'
+import React,{Suspense, useState} from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from '@heroui/react'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { resetPasswordSchema } from '@/zod/zod'
+import { authClient } from '@/auth-client'
+import { useToast } from "@/hooks/use-toast"
+import { useSearchParams } from 'next/navigation'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useRouter } from 'next/navigation'
+const ResetForm = () => {
+    const [pending, setPending] = useState(false);
+    const { toast } = useToast()
+    const router = useRouter()
+    const searchParams = useSearchParams();
+    const error = searchParams.get("error");
+    const token:any = searchParams.get("token");
+
+    console.log(token)
+
+        // 1. Define your form.
+        const form = useForm<z.infer<typeof resetPasswordSchema>>({
+          resolver: zodResolver(resetPasswordSchema),
+          defaultValues: {
+            newPassword:"",
+            confirmPassword:""
+          },
+        })
+        const {reset} = form;
+       
+        // 2. Define a submit handler.
+        async function onSubmit(values: z.infer<typeof resetPasswordSchema>) {
+           //we set the is pending to true
+            setPending(true);
+            //we parse in the email from the form to authClient.forgetPassword
+            const { error } = await authClient.resetPassword({
+              newPassword:values.newPassword,
+              token
+            });
+        //if error we show the toast with the error from the auth server
+            if (error) {
+              toast({
+                title: "Error",
+                description: error.message,
+                variant: "destructive",
+              });
+            } else {
+            //if success we show the success message
+              toast({
+                title: "Success",
+                description: "Password reset successful. Login to continue.",
+              });
+              router.push("/sign-in");
+            }
+            //we then set ispending to false
+            reset()
+            setPending(false);
+        }
+        if (error === "INVALID_TOKEN") {
+          //if error occurs in the search params we show that the token in invalid
+            return (
+              <div className="grow flex items-center justify-center p-4">
+                <Card className="w-full max-w-md">
+                  <CardHeader>
+                    <CardTitle className="text-3xl font-bold text-center text-gray-800">
+                      Invalid Reset Link
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="space-y-4">
+                      <p className="text-center text-gray-600">
+                        This password reset link is invalid or has expired.
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            );
+          }
+      
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        
+        <FormField
+          control={form.control}
+          name="newPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='font-bold text-[13px] text-slate-600 dark:text-gray-300'>New Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Atleast 8 characters" {...field} type='password'/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='font-bold text-[13px] text-slate-600 dark:text-gray-300'>Confirm Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Atleast 8 characters" {...field} type='password'/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+       
+        <Button isLoading={pending} className='bg-[#007AFF] w-full text-[15px] font-medium text-white' type="submit" radius='sm'>Change Password</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default function ResetPassword(){
+  return(
+    <Suspense>
+      <ResetForm></ResetForm>
+    </Suspense>
+  )
+}
