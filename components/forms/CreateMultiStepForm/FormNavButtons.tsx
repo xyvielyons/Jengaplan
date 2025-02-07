5c8b5e4c6dcc74ec40a3207edfbc7167a56b46de
xyvie lyons <xyvielyons@gmail.com>
2025-02-07 06:22:04 +0300

    fixing navbuttons to be responsive

--- components/forms/CreateMultiStepForm/FormNavButtons.tsx
+++ components/forms/CreateMultiStepForm/FormNavButtons.tsx
@@ -2,6 +2,7 @@ import { ChevronLast, ChevronLeft, ChevronRight } from "lucide-react";
 import React from "react";
 import { useAppSelector,useAppDispatch } from "@/hooks/hooks";
 import { setCurrentStep } from "@/store/slices/SchemeSlice";
+import { Button } from "@heroui/react";
 export default function NavButtons() {
   const currentStep = useAppSelector((store) => store.schemes.currentStep);
   const dispatch = useAppDispatch();
@@ -10,26 +11,26 @@ export default function NavButtons() {
   }
   
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between flex-col-reverse gap-2 md:flex-row">
       {currentStep > 1 && (
-        <button
-          onClick={handlePrevious}
-          type="button"
-          className="inline-flex items-center px-5 py-2 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-slate-900 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-slate-800 dark:bg-blue-600 dark:hover:bg-blue-700"
+        <Button
+          onPress={handlePrevious}
+          radius="sm"
         >
           <ChevronLeft className="w-5 h-5 mr-2" />
           <span>Previous</span>
-        </button>
+        </Button>
       )}
-      <button
+      <Button
         type="submit"
-        className="inline-flex items-center px-5 py-2 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-slate-900 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-slate-800 dark:bg-blue-600 dark:hover:bg-blue-700"
+        className="bg-blue-600 text-white"
+        radius="sm"
       >
         <span>
           {currentStep === 6 ? "Confirm and Submit" : "Save and Continue"}
         </span>
         <ChevronRight className="w-5 h-5 ml-2" />
-      </button>
+      </Button>
     </div>
   );
 }
